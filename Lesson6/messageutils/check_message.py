07c0d5f3ddbf14251c0e72e1787e96d727cf3b0a
ssergo99 <95188760+ssergo99@users.noreply.github.com>
2023-04-18 10:41:24 +0300

    DZ6

---
+++ Lesson6/messageutils/check_message.py
@@ -0,0 +1,30 @@
+import json
+from deco_func import log
+
+@log
+def check_msg(message):
+    if isinstance(message, bytes):
+        dec_message = message.decode('utf-8')
+        load_message = json.loads(dec_message)
+        if isinstance(load_message, dict):
+            return load_message
+        raise ValueError
+    raise ValueError
+
+@log
+def check_client_message(message):
+    if 'action' in message and message['action'] == 'presence' and 'time' in message \
+            and 'user' in message:
+        return {'response': 200}
+    return {
+        'response': 400,
+        'error': 'Bad Request'
+    }
+
+@log
+def serv_response_check(message):
+    if 'response' in message:
+        if message['response'] == 200:
+            return 'Success'
+        return f'400, error: {message["error"]}'
+    raise ValueError
